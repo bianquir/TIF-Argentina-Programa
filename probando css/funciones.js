0e152f6945ada2324e653d517c5de73f5ed821ec
Bianca Quiroga <128812164+bianquir@users.noreply.github.com>
2023-04-18 14:48:12 -0300

    formularios
    modifique formularios de contacto y agregue formulario de proceso, con js

--- probando css/funciones.js
+++ probando css/funciones.js
@@ -26,48 +26,105 @@ prevButton.addEventListener('click', () => {
   slidesContainer.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
 });
 
+//FORmularios
 
-//validacion de formulario de contacto
-const constraints = {
-    nombre: {
-    presence: true,
-    length: {
-    minimum: 2,
-    message: "debe tener al menos 2 caracteres"
-        }
-    },
-        apellido: {
-        presence: true,
-        length: {
-        minimum: 2,
-        message: "debe tener al menos 2 caracteres"
-            }
-        },
-            email: {
-            presence: true,
-            email: true
+document.addEventListener('DOMContentLoaded', function() {
+  // Configurar reglas de validación y mensajes de error usando jQuery validation
+  $('#formularioProceso').validate({
+      rules: {
+          nombreProceso: 'required',
+          art: 'required',
+          reclamo: 'required'
+      },
+      messages: {
+          nombreProceso: 'Por favor ingrese su nombre',
+          art:'Por favor ingrese el precio',
+          reclamo: 'Por favor ingrese la cantidad'
+      },
+      submitHandler: function(form) {
+          // Obtener los valores de los campos del formulario
+          var nombre = document.getElementById('nombreProceso').value;
+          var art = document.getElementById('art').value;
+          var reclamo = document.getElementById('reclamo').value;
+
+          // Generar el resumen de la cotización
+          var reclamo = 'Reclamo:\n\n' +
+              'Nombre: ' + nombre + '\n' +
+              'Art de reclamo: ' + art + '\n' +
+              'Detalle del reclamo: ' + reclamo+ '\n' ;
+
+          // Mostrar la cotización en un cuadro de diálogo
+          alert(reclamo);
+
+          // Continuar con el resto del código de generación del PDF y descarga del archivo, si es necesario
+          // ...
+          // Crear un nuevo objeto jsPDF
+          var pdf = new jsPDF();
+
+          // Agregar el resumen al documento PDF
+          pdf.text(reclamo, 10, 10);
+
+          // Generar el archivo PDF como Blob
+          var pdfBlob = pdf.output('blob');
+
+          // Crear un enlace de descarga
+          var downloadLink = document.createElement('a');
+          downloadLink.href = URL.createObjectURL(pdfBlob);
+          downloadLink.download = 'resumen_reclamo.pdf';
+          downloadLink.click();
+
+          // Liberar el objeto Blob
+          URL.revokeObjectURL(pdfBlob);
+      
+      }
+  });
+
+  // Configurar reglas de validación y mensajes de error usando jQuery validation para el formulario de contacto
+  $('#formularioContacto').validate({
+      rules: {
+          nombre: 'required',
+          email: {
+              required: true,
+              email: true
           },
-            mensaje: {
-            presence: true,
-            length: {
-                minimum: 10,
-                message: "debe tener al menos 10 caracteres"
-            }
-        }
-};
-    
-        const form = document.getElementById("formulario");
-    
-        form.addEventListener("submit", function (event) {
-          event.preventDefault();
-          const formData = new FormData(form);
-          const formValues = Object.fromEntries(formData.entries());
-          const errors = validate(formValues, constraints);
-          if (errors) {
-            alert(Object.values(errors).map(error => error.join("\n")).join("\n"));
-          } else {
-            alert("El formulario se envió correctamente");
-            // Aquí se podría agregar código para enviar el formulario a un servidor
-            form.reset();
-          }
-        });
+          mensaje: 'required'
+      },
+      messages: {
+          nombre: 'Por favor ingrese su nombre',
+          email: {
+              required: 'Por favor ingrese su dirección de correo electrónico',
+              email: 'Por favor ingrese una dirección de correo electrónico válida'
+          },
+          mensaje: 'Por favor ingrese un mensaje'
+      },
+      submitHandler: function(form) {
+          // Obtener los valores de los campos del formulario
+          var nombre = $('#nombre').val();
+          var email = $('#email').val();
+          var mensaje = $('#mensaje').val();
+
+          // Hacer la petición AJAX para enviar los datos al servidor
+          $.ajax({
+              url: 'https://reqres.in/api/users?page=2', // URL de regres.in para la petición de contacto
+              method: 'POST', // Método HTTP POST
+              data: {
+                  nombre: nombre,
+                  email: email,
+                  mensaje: mensaje
+              },
+              success: function(response) {
+                  // Aquí puedes manejar la respuesta del servidor si es necesario
+                  console.log('Éxito:', response);
+                  // Puedes mostrar un mensaje de éxito al usuario
+                  alert('¡Mensaje enviado con éxito!');
+              },
+              error: function(xhr, status, error) {
+                  // Aquí puedes manejar los errores de la petición AJAX si es necesario
+                  console.error('Error:', error);
+                  // Puedes mostrar un mensaje de error al usuario
+                  alert('Error al enviar el mensaje. Por favor inténtelo nuevamente.');
+              }
+          });
+      }
+  });
+});
